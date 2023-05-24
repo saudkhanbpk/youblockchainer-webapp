14f0430dcb10073699d452184ab9ce569ec20e75
Khushi Mehta <khushimehta8282@gmail.com>
2023-05-24 18:42:38 +0530

    setup main router and added some css

---
+++ src/theme/CssMy.js
@@ -0,0 +1,7 @@
+export const btn = { textTransform: 'none', fontSize: '14px', fontFamily: 'Poppins', color: '#3770FF', border: '2px solid #3770FF' }
+export const card = { backgroundColor: 'white', padding: '10px', borderRadius: '10px', height: '100%', margingBottom: '0%', paddingBottom: '0% !important' }
+export const text = { fontFamily: 'Poppins' }
+export const ptag = { fontSize: '12px', fontFamily: 'Poppins', color: '#6A707F', margin: '0', padding: '0' }
+export const df_jc_ac = { display: 'flex', justifyContent: 'center', alignItems: 'center' }
+export const df_jfe_ac = { display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }
+export const textField = { width: '100%', marginBottom: '2%', backgroundColor: 'white', boxShadow: '0px 1px 26px rgba(94, 99, 116, 0.05)' }
