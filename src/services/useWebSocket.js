5c24384016778593961ddadb8a2f1e01cb9eed0f
khushi mehta <khushimehta8282@gmail.com>
2023-06-22 13:21:22 +0530

    sessionstorage

--- src/services/useWebSocket.js
+++ src/services/useWebSocket.js
@@ -10,7 +10,7 @@ export const useWebSockets = ({ roomId, enabled, sender, expert }) => {
     const input = 'Input Chat Message';
     const output = 'Output Chat Message';
     const joinRoom = 'Join Room';
-    const user = JSON.parse(localStorage.getItem('ybUser'))
+    const user = JSON.parse(sessionStorage.getItem('ybUser'))
     const params = useParams()
     const id = params.id
     console.log(roomId, enabled, sender)
@@ -49,7 +49,7 @@ export const useWebSockets = ({ roomId, enabled, sender, expert }) => {
 
     const fetchFromDevice = async () => {
         try {
-            let res = localStorage.getItem(roomId);
+            let res = sessionStorage.getItem(roomId);
             // console.log(res, 'res')
             if (res === undefined) {
                 return [];
@@ -70,8 +70,8 @@ export const useWebSockets = ({ roomId, enabled, sender, expert }) => {
                 .map(i => backendtoChatScope(i, user._id, user, expert))
                 // .sort((a, b) => Date.parse(a.time) - Date.parse(b.time));
             setMessages(prior);
-            // localStorage.setItem('roomId', roomId);
-            localStorage.setItem(roomId, JSON.stringify(prior));
+            // sessionStorage.setItem('roomId', roomId);
+            sessionStorage.setItem(roomId, JSON.stringify(prior));
         } catch (e) {
             console.log('Error in getting previous chats:- ', e.message);
         }
